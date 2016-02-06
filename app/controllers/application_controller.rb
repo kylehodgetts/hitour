cf637760301b082147065a92b0781aa7b2cf10e0
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-06 19:16:10 +0000

    Users can now log in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  protected
+
+  def after_sign_in_path_for(_resource)
+    root_url
+  end
+
   def authenticate_user!
     if user_signed_in?
       super
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy', as: 'logout'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
