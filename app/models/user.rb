da8f10ce0e39f0f62acd8ee45ae3249fb79c6216
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-06 18:57:23 +0000

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -7,6 +8,14 @@ class User < ActiveRecord::Base
          :validatable,
          :omniauthable,
          omniauth_providers: [:google_oauth2]
+
+  before_create { self.encrypted_password = Devise.friendly_token[0, 20] }
+
+  before_save { self.email = email.downcase }
+
+  validates :email, presence: true,
+                    format:     { with: EMAIL_FORMAT },
+                    uniqueness: { case_sensitive: false }
 
   def self.from_omniauth(access_token)
     data = access_token.info
