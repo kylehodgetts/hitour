c3e924920e188d940d6b5cce59d95ec9c04ddfc7
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-07 02:15:58 +0000

    All tests pass

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,6 @@ module Users
     def google_oauth2
       # Method implemented in User model class
       @user = User.from_omniauth(request.env['omniauth.auth'])
-
       if @user
         sign_in_and_redirect @user, event: :authentication
       else
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
+  # How to mock a log in!
+  before(:each) do
+    @user = User.create(email: 'user@gmail.com')
+    allow_message_expectations_on_nil
+    allow(request.env['warden']).to receive(:authenticate!) { @user }
+    allow(controller).to receive(:current_user) { @user }
+  end
+
+  # Destroy user afterwards
+  after do
+    @user.destroy
+  end
+
+  describe 'GET #index' do
+    it 'responds successfully with an HTTP 200 status code' do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
 end
