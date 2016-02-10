e25554a68408becbba3c01c5d6a36b5fd258e607
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-10 02:33:57 +0000

    Set up component props

--- app/assets/javascripts/components/navigation/header.es6.jsx
+++ app/assets/javascripts/components/navigation/header.es6.jsx
@@ -3,13 +3,23 @@ class Header extends React.Component {
     return (
       <nav>
         <div className="nav-wrapper  blue darken-1">
-          <a href="#!" id="logo" className="brand-logo center">hiTour</a>
+          <a href={this.props.rootPath} id="logo" className="brand-logo center">hiTour</a>
           <a href="#" data-activates="mobile-nav" className="button-collapse"><i className="material-icons">menu</i></a>
           <ul className="right hide-on-med-and-down">
-            <Navlist />
+            <Navlist
+              toursPath = {this.props.toursPath}
+              pointsPath = {this.props.pointsPath}
+              usersPath = {this.props.usersPath}
+              logoutPath = {this.props.logoutPath}
+            />
           </ul>
           <ul className="side-nav" id="mobile-nav">
-            <Navlist />
+          <Navlist
+            toursPath = {this.props.toursPath}
+            pointsPath = {this.props.pointsPath}
+            usersPath = {this.props.usersPath}
+            logoutPath = {this.props.logoutPath}
+          />
           </ul>
         </div>
       </nav>
@@ -20,5 +30,9 @@ class Header extends React.Component {
 
 Header.displayName = "Header";
 Header.propTypes = {
-
+  rootPath: React.PropTypes.string.isRequired,
+  toursPath: React.PropTypes.string.isRequired,
+  pointsPath: React.PropTypes.string.isRequired,
+  usersPath: React.PropTypes.string.isRequired,
+  logoutPath: React.PropTypes.string.isRequired
 }
--- app/assets/javascripts/components/navigation/navlist.es6.jsx
+++ app/assets/javascripts/components/navigation/navlist.es6.jsx
@@ -2,13 +2,19 @@ class Navlist extends React.Component {
   render () {
     return (
       <div>
-        <li><a href="#">Test</a></li>
-        <li><a href="#">Points</a></li>
-        <li><a href="#">Users</a></li>
-        <li><a href="#">Logout</a></li>
+        <li><a href={this.props.toursPath}>Tours</a></li>
+        <li><a href={this.props.pointsPath}>Points</a></li>
+        <li><a href={this.props.usersPath}>Users</a></li>
+        <li><a href={this.props.logoutPath}>Logout</a></li>
       </div>
     );
   }
 }
 
 Navlist.displayName = "Navlist";
+Navlist.propTypes = {
+  toursPath: React.PropTypes.string.isRequired,
+  pointsPath: React.PropTypes.string.isRequired,
+  usersPath: React.PropTypes.string.isRequired,
+  logoutPath: React.PropTypes.string.isRequired
+}
