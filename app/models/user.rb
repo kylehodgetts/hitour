273925872c801c21f939b6566df242aa2de63370
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-10 14:21:41 +0000

    Removed omniauth methods from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,26 +6,11 @@ class User < ActiveRecord::Base
          :recoverable,
          :rememberable,
          :trackable,
-         :validatable,
-         :omniauthable,
-         omniauth_providers: [:google_oauth2]
+         :validatable
 
-  before_validation :generate_devise_password, on: :create
   before_save { self.email = email.downcase }
 
   validates :email, presence: true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
-  def self.from_omniauth(access_token)
-    data = access_token[:info]
-    user = User.find_by(email: data[:email])
-    user
-  end
-
-  private
-
-  def generate_devise_password
-    self.password = Devise.friendly_token[0..20]
-  end
 end
