ab13e0c4545494f80ff46df427a21a49f2d9ad01
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-12 12:16:06 +0000

    Adding email validation to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   has_secure_password
+  validates :email, presence: :true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   before_save { self.email = email.downcase }
 end
