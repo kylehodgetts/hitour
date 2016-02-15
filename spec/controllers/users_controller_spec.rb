fd221e538fd3355cac37874078e60f788a4dcf9b
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-15 14:12:09 +0000

    tests for methods

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,4 +12,27 @@ RSpec.describe UsersController, type: :controller do
       expect(user).to eq(@user)
     end
   end
+
+  describe 'GET #show' do
+    context 'while signed in' do
+      before(:each) do
+        User.delete(User.find_by(email: 'kyle@gmail.com'))
+        @user = User.create(email: 'kyle@gmail.com', password: 'password')
+        @user.save
+        @other_user = User.create(email: 'dude@gmail.com', password: 'password')
+        @other_user.save
+        session[:user_id] = @user.id
+      end
+
+      it 'should open own profile ' do
+        get :show, id: @user.id
+        expect(response).to have_http_status '200'
+      end
+
+      it 'should not be able to access other profiles via url' do
+        get :show, id: @other_user.id
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
