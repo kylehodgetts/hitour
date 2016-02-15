bb0e7ca56f80bdab1d7fc02c933e5821e945b45a
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-15 14:54:57 +0000

    tests for updating user password

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe UsersController, type: :controller do
     context 'while signed in' do
       before(:each) do
         User.delete(User.find_by(email: 'kyle@gmail.com'))
+        User.delete(User.find_by(email: 'dude@gmail.com'))
         @user = User.create(email: 'kyle@gmail.com', password: 'password')
         @user.save
         @other_user = User.create(email: 'dude@gmail.com', password: 'password')
@@ -32,6 +33,37 @@ RSpec.describe UsersController, type: :controller do
       it 'should not be able to access other profiles via url' do
         get :show, id: @other_user.id
         expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe 'POST #update' do
+    before(:each) do
+      User.delete(User.find_by(email: 'kyle@gmail.com'))
+      @user = User.create(email: 'kyle@gmail.com', password: 'password')
+      @user.save
+      session[:user_id] = @user.id
+    end
+    describe 'with password not matching confirm password' do
+      it 'should not update the user entry' do
+        password = 'new_password'
+        post :update, id: @user.id,
+                      user: @user,
+                      password: password,
+                      cpassword: 'password1'
+        expect(flash[:user_save]).to eq 'false'
+      end
+    end
+
+    describe 'with password matching confirm password' do
+      it 'should update the user entry' do
+        password = 'new_password'
+        post :update, id: @user.id,
+                      user: @user,
+                      password: password,
+                      cpassword: password
+        @user.save
+        expect(flash[:user_save]).to eq 'true'
       end
     end
   end
