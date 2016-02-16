51e06b494f32a2ff30d7cc9a626e1e3e8973b7fb
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-16 23:08:15 +0000

    removed unused files

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,10 +1,11 @@
 class PointsController < ApplicationController
+	include RQRCode
 	before_action :authenticate_user!
 	def index
 		@points = Point.includes(:data)
 		@points_qr = []
 		@points.each do |point|
-			@points_qr[point.id] = RQRCode::QRCode.new(point.id.to_s + '-' + point.name)
+			@points_qr[point.id] = QRCode.new('#{point.id.to_s} - #{point.name}')
 		end
 	end
 
@@ -16,7 +17,7 @@ class PointsController < ApplicationController
 			@ranks[datum_rank.datum.id] = datum_rank.rank
 		end
 		@data_audiences = Datum.includes(:audiences)
-		@qrcode = RQRCode::QRCode.new('#{@point.id.to_s} - #{@point.name}')
+		@qrcode = QRCode.new('#{@point.id.to_s} - #{@point.name}')
 	end
 
 	def edit
@@ -37,16 +38,10 @@ class PointsController < ApplicationController
 	end
 
 	def create
-		unless Point.find_by(name: params[:name])
-			@point = Point.new(point_params)
-			if @point.save
-				redirect_to @point
-			else
-				@message = 'The name of the point has already been taken.'
-				redirect_to new_point_path
-			end
+		if Point.find_by(name: params[:name])
+			render new
 		else
-				render new
+			create_point
 		end
 	end
 
@@ -54,5 +49,15 @@ class PointsController < ApplicationController
 
 	def point_params
 		params.require(:point).permit(:name)
+	end
+
+	def create_point
+		@point = Point.new(point_params)
+		if @point.save
+			redirect_to @point
+		else
+			@message = 'The name of the point has already been taken.'
+			redirect_to new_point_path
+		end
 	end
 end
--- app/helpers/tours_points_helper.rb
+++
@@ -1,2 +0,0 @@
-module ToursPointsHelper
-end
