435f0d43446f0ccc759d42a59fea9459a59ef52c
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-02-23 02:31:45 +0000

    Started on creating populated seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,86 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Datum.create([{title:'Test Data',description:'Test Description',
-						url:'http://www.w3schools.com/html/mov_bbb.mp4'}])
+# Audiences
+alevel = Audience.new(name: 'A-Level Student')
+uni_student = Audience.new(name: 'University Student')
 
-Point.create(name:"MRI Imaging Scanner")
+# Data - Also assigning it's audience
+datum1 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
-Point.create(name:"Angiography")
+datum2 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
-point3 = Point.create(name:"UltraSound")
+datum3 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
-datum = Datum.create(title:"UltraSText",description:"You're pregnant",url:"http://s.hswstatic.com/gif/mri-10.jpg")
-PointDatum.create(point_id:point3.id,datum_id:datum.id,rank:0)
+datum4 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
-alevelStudent = Audience.create(name:"A-Level")
-uniStudent = Audience.create(name:"Uni Student")
+datum5 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
-DataAudience.create(datum_id:datum.id,audience_id:alevelStudent.id)
+datum6 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
-DataAudience.create(datum_id:datum.id,audience_id:uniStudent.id)
+datum7 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
-Tour.create([{name:"Imaging Tour",audience_id:uniStudent.id}])
+datum8 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
-User.create(email:"dev@mail.com",password:"password")
+datum9 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
 
+datum10 = Datum.create(title: 'Test Data', description: 'Test Description', url: 'http://www.w3schools.com/html/mov_bbb.mp4')
+DataAudience.create(datum_id: datum1.id, audience_id: alevel.id)
+DataAudience.create(datum_id: datum2.id, audience_id: alevel.id)
+
+# Points
+point1 = Point.create(name: 'Fluoroscopy')
+point2 = Point.create(name: 'Angiography')
+point3 = Point.create(name: 'Intravenous Urograms (IVU)')
+point4 = Point.create(name: 'Magnetic Resonance Imaging (MRI)')
+point5 = Point.create(name: 'Nuclear Medicine')
+
+# Point Data
+# For Point 1
+PointDatum.create(point_id: point1.id, datum_id: datum1.id)
+PointDatum.create(point_id: point1.id, datum_id: datum2.id)
+
+# For Point 2
+PointDatum.create(point_id: point2.id, datum_id: datum3.id)
+PointDatum.create(point_id: point2.id, datum_id: datum4.id)
+
+# For Point 3
+PointDatum.create(point_id: point3.id, datum_id: datum5.id)
+PointDatum.create(point_id: point3.id, datum_id: datum6.id)
+
+# For Point 4
+PointDatum.create(point_id: point4.id, datum_id: datum7.id)
+PointDatum.create(point_id: point4.id, datum_id: datum8.id)
+
+# For Point 5
+PointDatum.create(point_id: point5.id, datum_id: datum9.id)
+PointDatum.create(point_id: point5.id, datum_id: datum10.id)
+
+# Tours - With Point assignments
+tour1 = Tour.new(name: 'Imaging Tour: A-Level', audience_id: alevel.id)
+TourPoint.create(tour_id: tour1.id, point_id: point1.id, rank: '1')
+TourPoint.create(tour_id: tour1.id, point_id: point2.id, rank: '2')
+TourPoint.create(tour_id: tour1.id, point_id: point3.id, rank: '3')
+
+tour2 = Tour.new(name: 'Imaging Tour: University', audience_id: uni_student.id)
+TourPoint.create(tour_id: tour2.id, point_id: point4.id, rank: '1')
+TourPoint.create(tour_id: tour2.id, point_id: point5.id, rank: '2')
+TourPoint.create(tour_id: tour2.id, point_id: point6.id, rank: '3')
+
+# User Related Stuff
+User.create(email: 'dev@mail.com', password: 'password')
