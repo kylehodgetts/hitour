e05dc12f44ae8c43ca5598409abec66fc599788a
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-27 17:11:15 +0000

    Can now click on header tag to go into edit mode

--- app/assets/javascripts/components/data/data_viewer.es6.jsx
+++ app/assets/javascripts/components/data/data_viewer.es6.jsx
@@ -3,7 +3,6 @@ class DataViewer extends React.Component {
     var videoExtensions = ["mp4","avi","mov","avchd","flv","mpg","mpeg-4","wmv"];
     for(var i = 0;i < videoExtensions.length;i++){
       if((this.props.url).toLowerCase().indexOf(videoExtensions[i]) > -1){
-        console.log('Contains: '+videoExtensions[i]);
         return true;
       }
     }
@@ -30,12 +29,9 @@ class DataViewer extends React.Component {
       return;
     }
     if(this.checkIfImage()){
-      console.log('Is an image');
       var img = $('<img />').attr('src',url).addClass("responsive-img");
       datum_preview.append(img);
     }else if(this.checkIfVideo()){
-      console.log('Is an video');
-        console.log('Adding video');
         var video = $('<video />').addClass("responsive-video");
         video.append($('<source />').attr('src',url));
         video.attr('controls','');
@@ -58,4 +54,3 @@ class DataViewer extends React.Component {
 };
 
 DataViewer.displayName = "DataViewer";
-
