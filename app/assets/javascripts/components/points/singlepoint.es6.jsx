42e9573cff0e6f76729f4bb91149ea1e86c68bc0
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-02-28 14:37:05 +0000

    Updated point show component to use card reveal.

--- app/assets/javascripts/components/points/singlepoint.es6.jsx
+++ app/assets/javascripts/components/points/singlepoint.es6.jsx
@@ -32,11 +32,11 @@ class SinglePoint extends React.Component {
       cache: false,
       success: function(data){
         //Sets Points Cover Photo as background
-        $('body').css('background-image',"url("+"'"+data.point.url+"'"+")");
-        $('body').css('background-size','cover');
-        $('body').css('background-repeat','no-repeat');
-        $('body').css('background-postion','center center');
-        $('.point-card').css('opacity','0.92');
+        // $('body').css('background-image',"url("+"'"+data.point.url+"'"+")");
+        // $('body').css('background-size','cover');
+        // $('body').css('background-repeat','no-repeat');
+        // $('body').css('background-postion','center center');
+        // $('.point-card').css('opacity','0.92');
         var qrCode = $(data.qr_code);
         $('.point-qr-holder').html(qrCode);
         this.setState({
@@ -91,24 +91,33 @@ class SinglePoint extends React.Component {
   render() {
     var _this = this;
     return (
-      <div className="card-panel point-card">
-        <div className="row">
-          <div className="col s12">
-            {this.state.point.name &&
-            <GenericEdit value={this.state.point.name}
-                         postUrl={this.props.update_point_url}
-                         attributeName="point[name]"/>
-            }
-          </div>
-          <div className="col s12">
-            {this.state.point.description &&
-            <GenericEdit value={this.state.point.description}
-                         postUrl={this.props.update_point_url}
-                         attributeName="point[description]"/>
-            }
-          </div>
+      <div className="card large point-card" style={{height: '700px'}} >
+        <div className="card-image waves-effect waves-block waves-light">
+          {this.state.point.url &&
+            <DataViewer url={this.state.point.url} />}
         </div>
-        <div className="row">
+        <div className="card-content">
+          <div className="row">
+            <div className="col s12">
+              <span className="card-title activator grey-text text-darken-4">
+              {this.state.point.name &&
+              <GenericEdit value={this.state.point.name}
+                           postUrl={this.props.update_point_url}
+                           attributeName="point[name]"/>
+              }
+              <i className="material-icons right">more_vert</i></span>
+            </div>
+            <div className="col s12">
+              {this.state.point.description &&
+              <GenericEdit value={this.state.point.description}
+                           postUrl={this.props.update_point_url}
+                           attributeName="point[description]"/>
+              }
+            </div>
+        </div>
+        </div>
+        <div className="card-reveal">
+            <span className="card-title grey-text text-darken-4"><i className="material-icons right">close</i></span>
           <div className="point-qr-holder center-align">
           </div>
           <ul className="collapsible" data-collapsible="accordion">
@@ -145,12 +154,12 @@ class SinglePoint extends React.Component {
               );
             }, this)}
           </ul>
-        </div>
-        <NewPointDatum
-          point_id={this.props.point_id}
-          data_url={this.props.data_url}
-          new_point_datum_url={this.props.new_point_datum_url}
+          <NewPointDatum
+            point_id={this.props.point_id}
+            data_url={this.props.data_url}
+            new_point_datum_url={this.props.new_point_datum_url}
           />
+        </div>
       </div>
     );
   }
