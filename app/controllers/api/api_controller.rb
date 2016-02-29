93b254fcb5afdc5a5489336e047092e675693420
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-29 14:06:42 +0000

    Tidied up API module

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,70 +2,46 @@ module Api
   class ApiController < ApplicationController
     before_action :api_authenticate!
 
-    def audiences
-      render json: Audience.all
-    end
-
     def tours
       response = {}
       response['tours'] = Tour.all.as_json
       response['tours'].each do |tour|
-        tour['points'] = TourPoint.where(tour_id: tour['id']).as_json
-        tour['points'].each do |point|
-          pd = PointDatum.where(point_id: point['id']).as_json
-          point['data'] = []
-          pd.each do |point_datum|
-            data = Datum.where(id: point_datum['datum_id'])
-            data.each do |datum|
-              audiences = datum.audiences
-              audiences.each do |audience|
-                if audience.id == tour['audience_id']
-                  point['data'] << datum
-                  break
-                end
-              end
-            end
+        populate_tour_reponse(tour)
+      end
+
+      render json: response
+    end
+
+    def single_tour
+      response = Tour.find(params[:id]).as_json
+      response = populate_tour_reponse(response)
+      render json: response
+    end
+
+    private
+
+    def populate_tour_reponse(tour)
+      tour['points'] = TourPoint.where(tour_id: tour['id']).as_json
+      tour['points'].each do |point|
+        pd = PointDatum.where(point_id: point['id']).as_json
+        point['data'] = []
+        pd.each do |point_datum|
+          populate_data_reponse(tour, point, point_datum)
+        end
+      end
+    end
+
+    def populate_data_reponse(tour, point, point_datum)
+      data = Datum.where(id: point_datum['datum_id'])
+      data.each do |datum|
+        audiences = datum.audiences
+        audiences.each do |audience|
+          if audience.id == tour['audience_id']
+            point['data'] << datum
+            break
           end
         end
       end
-
-      render json: response
-    end
-
-    def tour
-      response = Tour.find(params[:id]).as_json
-      response['points'] = TourPoint.where(tour_id: response['id']).as_json
-      response['points'].each do |point|
-        point['data'] = PointDatum.where(point_id: point['id'])
-                                  .select do |datum|
-                                    DataAudience.where(datum_id: datum['id'])
-                                  end
-                                  .as_json
-        point['data'].each do |datum|
-          datum['audiences'] = DataAudience.where(datum_id: datum['id']).as_json
-        end
-      end
-      render json: response
-    end
-
-    def points
-      render json: Point.all
-    end
-
-    def data
-      render json: Datum.all
-    end
-
-    def tour_points
-      render json: TourPoint.all
-    end
-
-    def point_data
-      render json: PointDatum.all
-    end
-
-    def data_audiences
-      render json: DataAudience.all
     end
   end
 end
