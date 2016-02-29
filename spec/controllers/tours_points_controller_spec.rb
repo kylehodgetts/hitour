0d0da0bce0b0c35f55d1ea19ab41a8dada350ece
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-02-29 14:22:45 +0000

    Removed redundant tests

--- spec/controllers/tours_points_controller_spec.rb
+++ spec/controllers/tours_points_controller_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe ToursPointsController, type: :controller do
-
-  describe "GET #new" do
-    it "returns http success" do
-      create_user_session
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
