8ab150cdf69d4ffe29e9442789e968cb17602df2
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-02-29 13:48:58 +0000

    Query now returns data that should show to a particular audience

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -12,7 +12,20 @@ module Api
       response['tours'].each do |tour|
         tour['points'] = TourPoint.where(tour_id: tour['id']).as_json
         tour['points'].each do |point|
-          point['data'] = PointDatum.where(point_id: point['id']).as_json
+          pd = PointDatum.where(point_id: point['id']).as_json
+          point['data'] = []
+          pd.each do |point_datum|
+            data = Datum.where(id: point_datum['datum_id'])
+            data.each do |datum|
+              audiences = datum.audiences
+              audiences.each do |audience|
+                if audience.id == tour['audience_id']
+                  point['data'] << datum
+                  break
+                end
+              end
+            end
+          end
         end
       end
 
@@ -24,6 +37,13 @@ module Api
       response['points'] = TourPoint.where(tour_id: response['id']).as_json
       response['points'].each do |point|
         point['data'] = PointDatum.where(point_id: point['id'])
+                                  .select do |datum|
+                                    DataAudience.where(datum_id: datum['id'])
+                                  end
+                                  .as_json
+        point['data'].each do |datum|
+          datum['audiences'] = DataAudience.where(datum_id: datum['id']).as_json
+        end
       end
       render json: response
     end
