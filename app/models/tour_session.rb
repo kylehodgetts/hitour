54b262b014f923b4e556ef3c9ae1a1048e83f76d
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-01 19:17:18 +0000

    Replaced model validation

--- app/models/tour_session.rb
+++ app/models/tour_session.rb
@@ -2,10 +2,10 @@ class TourSession < ActiveRecord::Base
     PASSPHRASE_REGEX = /\A[A-z]+\z/i
     belongs_to :tour
 
-    # validates :name, presence: :true
-    # validates :passphrase, presence: :true,
-    #                        format: { with: PASSPHRASE_REGEX },
-    #                        length: { minimum: 5 }
+    validates :name, presence: :true
+    validates :passphrase, presence: :true,
+                           format: { with: PASSPHRASE_REGEX },
+                           length: { minimum: 5 }
 
     auto_strip_attributes :passphrase, squish: true
 end
