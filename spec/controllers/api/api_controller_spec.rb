72af420dad09c935b7b43ba9c882f33d1f85477d
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-02 14:46:13 +0000

    removed redundant test

--- spec/controllers/api/api_controller_spec.rb
+++ spec/controllers/api/api_controller_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Api::ApiController, type: :controller do
-  describe 'GET #audiences' do
-    it 'returns http success' do
-      get :audiences, access_key: '1234'
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
