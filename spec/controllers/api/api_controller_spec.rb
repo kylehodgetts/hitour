3bde45091fb391d280fa7c41b9d74234a01f751e
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-04 15:18:40 +0000

    Start testing for API responses to be completed in another PR

--- spec/controllers/api/api_controller_spec.rb
+++ spec/controllers/api/api_controller_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Api::ApiController, type: :controller do
+  before :each do
+    @audience = Audience.create(name: 'A-Level Student')
+    @tour = Tour.create(name: 'Imaging Tour: A-Level',
+                        audience_id: @audience.id)
+    @ts = TourSession.create(name: 'TourSession', tour_id: @tour.id,
+                             passphrase: 'passphrase',
+                             start_date: Date.current, duration: '1')
+  end
+  describe 'with a valid passphrase' do
+    it 'returns the tour associated with that passphrase' do
+      get :single_tour, access_key: ENV['ACCESS_KEY'],
+                        passphrase: @ts.passphrase
+      parsed_response = JSON.parse(response.body)['tours']
+      expect(parsed_response).to be_truthy
+    end
+  end
+
+  # Test that error is returned with invalid passphrase
+  # Test that when a valid passphrase has expired, error is returned
 end
