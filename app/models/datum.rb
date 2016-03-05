7affcf2655c7befe0c88d98c331ea87f2c0ba984
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-05 16:41:42 +0000

    Datum tests

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,5 +1,6 @@
 class Datum < ActiveRecord::Base
-	validates :title, uniqueness: true
+	validates :title, presence: :true, uniqueness: true
+	validates :description, presence: :true
 	has_many :points, through: :point_data
 	has_many :audiences, through: :data_audiences
 	has_many :data_audiences
