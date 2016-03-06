43e58d4986fd2c86a122dea1265458e25c7bedf9
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-06 13:24:22 +0000

    Removed breaking validaitons

--- app/models/data_audience.rb
+++ app/models/data_audience.rb
@@ -1,9 +1,4 @@
 class DataAudience < ActiveRecord::Base
 	belongs_to :data
 	belongs_to :audience
-
-	validates :data, presence: :true
-	validates :audience, presence: :true
-
-	attr_accessor :id
 end
