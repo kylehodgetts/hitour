f626f2352f124968473bb8dc3803dab75b06cb48
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-06 13:22:07 +0000

    DataAudience tets

--- app/models/data_audience.rb
+++ app/models/data_audience.rb
@@ -1,4 +1,9 @@
 class DataAudience < ActiveRecord::Base
 	belongs_to :data
 	belongs_to :audience
+
+	validates :data, presence: :true
+	validates :audience, presence: :true
+
+	attr_accessor :id
 end
