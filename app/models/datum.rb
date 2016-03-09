f0516d3184b2f8b8fdd0245993739eca4c1027f3
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-09 16:38:23 +0000

    Deleting a datum will remove datum from all points that include it

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,7 +1,7 @@
 class Datum < ActiveRecord::Base
 	validates :title, presence: :true, uniqueness: true
 	validates :description, presence: :true
-	has_many :points, through: :point_data
+	has_many :points, through: :point_data, dependent: :destroy
 	has_many :audiences, through: :data_audiences
 	has_many :data_audiences
 	has_many :point_data
