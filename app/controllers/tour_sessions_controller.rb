7f10e40c9c882dd69f5579b73390047d8862cd3e
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-03-09 16:51:50 +0000

    Created ability to send invitation for a specific tour session. Also included QR code in the email. Still need to add email function to Session function

--- app/controllers/tour_sessions_controller.rb
+++ app/controllers/tour_sessions_controller.rb
@@ -1,5 +1,6 @@
 class TourSessionsController < ApplicationController
   before_action :authenticate_user!
+  include RQRCode
   def create
     # Generate random passphrase
     params[:tour_session][:passphrase] = RandomWord.nouns.next
@@ -29,6 +30,42 @@ class TourSessionsController < ApplicationController
     tour_session = TourSession.find(params[:id])
     tour_session.destroy
     render json: ['Succesfully deleted tour session']
+  end
+
+  def send_email
+    tour_session = TourSession.find(params[:id])
+    render json: session_invitation(params[:email], tour_session)
+  end
+
+  def session_invitation(to, tour_session)
+    email = SendGrid::Mail.new do |m|
+     m.to      = to
+     m.from    = 'invitation@Hitour.com'
+     m.subject = 'HiTour - Invitation'
+     m.html =  generate_message(tour_session)
+    end
+    $sendgrid.send(email)
+  end
+
+  def generate_message(tour_session)
+    title = '<p>Dear Receipient, </p>'
+    regards = '<br><br><br><p>Kind Regards, <br>The HiTour Team</p><br>'
+    android_link = '<a>Download Android App</a><br>'
+    ios_link = '<a>Download IOS App</a><br>'
+    title + session_message(tour_session) + regards + android_link + ios_link
+  end
+
+  def session_message(tour_session)
+    date = tour_session.start_date.to_formatted_s(:long_ordinal)
+    message = "<p>You have been invited to take part in the
+    <b>#{tour_session.tour.name}</b>.<br>"
+    message += "<br>Please scan the QR code below or enter the passphrase
+    <b>#{tour_session.passphrase}</b> into the mobile app."
+    session_info = "<p>The contents of the tour will be available for
+    <b>#{tour_session.duration} days</b>
+    starting on <b>#{date}</b>.</p>"
+    svg = RQRCode::QRCode.new('SESSION-' + tour_session.passphrase).as_svg
+    message + session_info + svg
   end
 
   private
