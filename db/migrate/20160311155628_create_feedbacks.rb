9f4e322d54f15642a7f6c4be249910da51830137
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-11 16:09:25 +0000

    Added missing files

--- db/migrate/20160311155628_create_feedbacks.rb
+++ db/migrate/20160311155628_create_feedbacks.rb
@@ -1,7 +1,8 @@
 class CreateFeedbacks < ActiveRecord::Migration
   def change
     create_table :feedbacks do |t|
-
+      t.text :content
+      t.integer :rating
       t.timestamps null: false
     end
   end
