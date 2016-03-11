0bb2702e24d580ae6b7aa4aeca0f02affa139847
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-03-11 02:00:34 +0000

    Removed class for Mailing, due to too much cluttered html in strings. Created indivual view files for each email. Clean code :)

--- app/controllers/password_reset_controller.rb
+++ app/controllers/password_reset_controller.rb
@@ -1,19 +1,24 @@
 class PasswordResetController < ApplicationController
-  require 'mailing'
-  def new
-  end
-
   def create
   	@user = User.find_by_email(params[:email])
-  	if @user
-  		reset_password = SecureRandom.hex(25)
-	    @user.update_attribute(:temporarypassword,reset_password)
-      mailingReset = Mailing.new(@user,request.original_url)
-      mailingReset.reset_password
+    if @user
+      reset_password = SecureRandom.hex(25)
+      @user.update_attribute(:temporarypassword, reset_password)
+      send_reset_email
     	redirect_to root_path
-	  else
-	  redirect_to password_reset_path
-  	end
+    else
+      redirect_to password_reset_path
+    end
   end
 
+  def send_reset_email
+    @url = root_url
+    email = SendGrid::Mail.new do |m|
+     m.to      = @user.email
+     m.from    = 'services@Hitour.com'
+     m.subject = 'HiTour - Password Reset'
+     m.html = render_to_string(action: 'reset_template', layout: false)
+    end
+    sendgrid.send(email)
+  end
 end
