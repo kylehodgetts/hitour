16f47987c95e3d822a656f738670aa9c1460fcbf
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-03-12 12:01:57 +0000

    Created tests for feedback, but returns very strange error.

--- spec/controllers/feedback_controller_spec.rb
+++ spec/controllers/feedback_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe FeedbackController, type: :controller do
-
+  # describe 'POST #create' do
+  #   before(:each) do
+  #     create_user_session
+  #   end
+  #   describe 'create feedback' do
+  #     it 'with valid parameters ' do
+  #       # Create a tour session
+  #       tour = create_tour
+  #       post :create, feedback: {
+  #         tour_id: tour.id,
+  #         comment: 'Amazing Tour',
+  #         rating: 4
+  #       }
+  #       # Check tour session was created
+  #       expect(response).to eq 'Succesfully saved Feedback!'
+  #     end
+  #   end
+  # end
 end
