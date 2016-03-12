63ae3f3ba333bdffb87eefbb371b9a59783a2ce7
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-03-12 15:10:21 +0000

    Refractored code for star rating, and made it into a seperate component

--- app/assets/javascripts/components/tours/NewFeedback.es6.jsx
+++ app/assets/javascripts/components/tours/NewFeedback.es6.jsx
@@ -1,6 +1,6 @@
 class NewFeedback extends React.Component{
   componentDidMount () {
-    this.starListener();
+    // this.starListener();
     var postUrl = this.props.postUrl;
     $('#feedbackForm').on('submit',function(e){
       e.preventDefault();
@@ -32,17 +32,10 @@ class NewFeedback extends React.Component{
         <form id="feedbackForm" className="col s12" style={{border:'1px solid black'}}>
           <h3>Add Feedback</h3>
           <input type="hidden" name="feedback[tour_id]" value={this.props.tourId}></input>
-          <div className="row" style={{marginBottom:'0'}}>
-            <div className="col s12 start-container">
-              <label>Tour Rating:</label><br/>
-              <p className="star-rating"><span className="star material-icons">star_rate</span></p>
-              <p className="star-rating"><span className="star material-icons">star_rate</span></p>
-              <p className="star-rating"><span className="star material-icons">star_rate</span></p>
-              <p className="star-rating"><span className="star material-icons">star_rate</span></p>
-              <p className="star-rating"><span className="star material-icons">star_rate</span></p>
-              <input type="hidden" name="feedback[rating]" className="validate"/>
-            </div>
-          </div>
+          <StarRating
+            formName="feedback[rating]"
+            numberOfStars={5}
+          />
           <div className="row">
             <div className="input-field col s12">
               <textarea id="feedback[comment]" className="validate materialize-textarea" name="feedback[comment]"></textarea>
@@ -55,31 +48,6 @@ class NewFeedback extends React.Component{
         </form>
       </div>
     );
-  }
-
-  starListener () {
-    $('.star').on('click',function(e){
-      $('.star').removeClass('star-selected');
-      var currentIndex = $('.star').index(this);
-      for(var i = 0;i <= currentIndex;i++){
-        var star = $('.star').eq(i);
-        star.addClass('star-selected');
-      }
-      //Set the Hidden input
-      $(this).parent().parent().find('input').val(currentIndex+1);
-    });
-    $('.star').mouseenter(function(){
-      $('.star').removeClass('star-hover');
-      var currentIndex = $('.star').index(this);
-      for(var i = 0;i <= currentIndex;i++){
-        var star = $('.star').eq(i);
-        star.addClass('star-hover');
-      }
-    }).mouseleave(function(){
-      $('.star').removeClass('star-hover');
-    });
-    //Click first star
-    $('.star').eq(0).trigger('click');
   }
 }
 
