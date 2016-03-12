e270d5ce865b2a15518ba0f2ec8c61be2d8e8f93
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-03-12 21:35:10 +0000

    Created update, create, destroy actions for Quiz and Question controller

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,8 +1,26 @@
 class QuestionController < ApplicationController
   def create
+    params[:rank] = max_rank
+    question = Question.params(question_params)
+    if question.save
+      render json: ['Succesfully created question']
+    else
+      render json: [question.errors.full_messages.first]
+    end
+  end
+
+  def update
+    question = Question.find(params[:id])
+    if question.update_attributes(question_params)
+      render json: ['Updated question!']
+    else
+      render json: [question.errors.full_messages.first]
+    end
   end
 
   def destroy
+    Question.find(params[:id]).delete
+    render json: ['Deleted question']
   end
 
   private
--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,8 +1,17 @@
 class QuizController < ApplicationController
   def create
-    quiz = Quiz.new(quizzes_params)
+    quiz = Quiz.new(quiz_params)
     if quiz.save
       render json: ['Succesfully created quiz']
+    else
+      render json: [quiz.errors.full_messages.first]
+    end
+  end
+
+  def update
+    quiz = Quiz.find(params[:id])
+    if quiz.update_attributes(quiz_params)
+      render json: ['Successfully updated Quiz'], status: 200
     else
       render json: [quiz.errors.full_messages.first]
     end
@@ -15,7 +24,7 @@ class QuizController < ApplicationController
 
   private
 
-  def quizzes_params
+  def quiz_params
     params.require(:quiz).permit(:name)
   end
 end
