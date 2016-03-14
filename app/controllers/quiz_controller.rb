90bf296cbb597e05be0be74b4c92c00ee5c1415d
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-03-14 23:37:21 +0000

    Added question to session to prevent updating database on re-submission

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -21,15 +21,18 @@ class QuizController < ApplicationController
 
   def submit_question
     return render json: ['No answer'] if params[:answer].nil?
+    session[:questions] ||= []
     answer = Answer.find(params[:answer][:id])
+    question_id = params[:question][:id]
     if answer.is_correct
-      increment_question_correct(params[:question][:id])
+      increment_question_correct(question_id)
       render json: { correct: true }
     else
-      increment_question_wrong(params[:question][:id])
-      answer = correct_answer(params[:question][:id])
+      increment_question_wrong(question_id)
+      answer = Answer.where(question_id: question_id, is_correct: true).first
       render json: { correct: false, answer: answer.as_json }
     end
+    session[:questions] << question_id
   end
 
   def correct_answer(question_id)
@@ -37,15 +40,19 @@ class QuizController < ApplicationController
   end
 
   def increment_question_correct(question_id)
-    question = Question.find(question_id)
-    current = question.correctly_answered
-    question.update_attribute(:correctly_answered, current + 1)
+    unless session[:questions].include?question_id
+      question = Question.find(question_id)
+      current = question.correctly_answered
+      question.update_attribute(:correctly_answered, current + 1)
+    end
   end
 
   def increment_question_wrong(question_id)
-    question = Question.find(question_id)
-    current = question.wrongly_answered
-    question.update_attribute(:wrongly_answered, current + 1)
+    unless session[:questions].include?question_id
+      question = Question.find(question_id)
+      current = question.wrongly_answered
+      question.update_attribute(:wrongly_answered, current + 1)
+    end
   end
 
   def quiz_data(tour_id)
