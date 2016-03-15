bb50950ebcde60dc798e988fb2d48b82c7647bf3
Tahmidul Islam <k1462015@kcl.ac.uk>
2016-03-15 15:09:51 +0000

    Changed audience selector to select tag instead of radio buttons

--- app/assets/javascripts/components/data/Data.es6.jsx
+++ app/assets/javascripts/components/data/Data.es6.jsx
@@ -1,9 +1,12 @@
 class Data extends React.Component {
+  componentDidMount () {
+    $('.audienceSelector').material_select();
+  }
   render () {
     return (
       <div>
         <GenericList getUrl={this.props.getUrl} />
-        <form onSubmit={this.showProgressBar} id="datumForm" className="col s12" encType="multipart/form-data" action={this.props.postUrl} method="post" >
+        <form style={{marginBottom: '100px'}} onSubmit={this.showProgressBar} id="datumForm" className="col s12" encType="multipart/form-data" action={this.props.postUrl} method="post" >
                   <div className="row">
             <div className="input-field col s12">
                 <input id="datum[title]" type="text" name="title" className="validate" required/>
@@ -25,20 +28,17 @@ class Data extends React.Component {
               <input className="file-path validate" type="text" placeholder="Upload your file here" />
             </div>
           </div>
-          <p>Initial Audience Datum is available to:</p>
-          <div style={{height:'200px', overflowY:'scroll'}}>
-          {this.props.audiences.map(function(audience, index) {
-            return (
-              <p key={index}>
-                <input value={audience.id}
-                       defaultChecked = {index == 0}
-                       name="datum[audience]"
-                       type="radio"
-                       id={"audience-"+audience.id}/>
-                <label htmlFor={"audience-"+audience.id}>{audience.name}</label>
-              </p>
-            )
-          })}
+          <div className="row">
+            <div className="input-field col s12">
+              <select id="datum[audience]" className="audienceSelector" name="datum[audience]">
+                {this.props.audiences.map(function(audience) {
+                  return (
+                    <option value={audience.id} key={audience.id} >{audience.name}</option>
+                  );
+                }, this)}
+              </select>
+              <label htmlFor="datum[audience]">Initial Audience Datum is available to:</label>
+            </div>
           </div>
           <label>Note: More Audiences can be added later</label>
           <button className="btn right blue waves-effect waves-light"
