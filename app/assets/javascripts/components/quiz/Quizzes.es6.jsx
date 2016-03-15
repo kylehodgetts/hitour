d809c4c70f1b16d8780e1fa1f76622c940d377a4
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-15 16:40:20 +0000

    Tidied common components into common folder. moved password reset into users folder

---
+++ app/assets/javascripts/components/quiz/Quizzes.es6.jsx
@@ -0,0 +1,47 @@
+class Quizzes extends React.Component {
+  componentDidMount() {
+    var postURL = this.props.postUrl;
+    $('#userForm').on('submit',function(e){
+      e.preventDefault();
+      $('.progress-message').text('Creating User. Please wait...');
+      $('.progress-overlay').fadeIn(200);
+      $.ajax({
+        url: postURL,
+        type: "POST",
+        data: $(this).serialize(),
+        success: function(data){
+          $('.progress-overlay').fadeOut();
+          Materialize.toast(data, 3000, 'rounded');
+          $('#userForm').trigger("reset");
+        },
+        error: function(err){
+          console.log("Error" + err);
+          console.log(err);
+          Materialize.toast('ERR: Succesfully added new user!', 3000, 'rounded');
+        }
+      });
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <GenericList getUrl={this.props.getUrl} users />
+        <form id="userForm">
+          <label htmlFor="user[email]">User Email</label>
+          <input type="text" name="user[email]" id="user[email]" />
+          <button className="btn right blue waves-effect waves-light"
+                  type="submit" name="action">Submit
+            <i className="material-icons right">send</i>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+Quizzes.displayName = "Quizzes";
+Quizzes.propTypes = {
+  getUrl: React.PropTypes.string.isRequired,
+  postUrl: React.PropTypes.string.isRequired
+}
