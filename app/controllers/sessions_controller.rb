67f49152f208928f5d4e2923863ac388a42c9285
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-15 02:03:52 +0000

    Session controller documenting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
+# Version 1.1
+# Sessions Controller responsible for handling users logging
+# in and out from the CMS
 class SessionsController < ApplicationController
+  # If user is already logged in
+  # redirect them from the log in page to main page
   def new
     redirect_to root_path if session[:user_id]
   end
 
-  # Log a user in
+  # Create a user session
   def create
     # Find the user in the database by the email given
     @user = User.find_by_email(params[:email].downcase)
@@ -24,20 +29,23 @@ class SessionsController < ApplicationController
 
   private
 
+  # Called when a user has been successfully authenticated
+  # Sets the current session's user id to the authenticated user
   def create_user_session
     # Create a user session and redirect to main page
     session[:user_id] = @user.id
-    if !@user.activated
-      redirect_to update_profile_path(@user.id)
-    else
+    if @user.activated
       redirect_to root_path
+    else
+      redirect_to update_profile_path(@user.id)
     end
   end
 
+  # Called when the user's password is equal to the temporarypassword
+  # Reset the password of the user to the
+  # temporary password and remove the previously
+  # given temporary password.
   def set_temporary_password
-    # Reset the password of the user to the
-    # temporary password and remove the previously
-    # given temporary password.
     @user.update_attribute(:password, params[:password])
     @user.update_attribute(:temporarypassword, '')
     @user.authenticate(params[:password])
