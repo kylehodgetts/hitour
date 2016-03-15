89fa4eb1a71ff6e5defa7d64b53e9fc157c4d246
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-15 03:12:03 +0000

    Fixed log in issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token, only: [:login, :logout]
+  skip_before_action :verify_authenticity_token
   helper_method :authenticate_user!, :current_user,
                 :api_authenticate!, :api_response
   # Return current user or nil, if a user is not logged in
