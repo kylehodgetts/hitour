df8f84efc55c46e6bfa7d805bbe28d24650ae9de
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-17 00:31:39 +0000

    Points Controller docs

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,7 +1,12 @@
+# Version 1.0
+# Points Controller response for creating and manipulating
+# point records that appear in tours
 class PointsController < ApplicationController
 	include RQRCode
 	before_action :authenticate_user!
 
+	# Prepare all point records for response via the
+	# hiTour API
 	def index
 		@points = Point.includes(:data)
 		@points_qr = []
@@ -20,6 +25,9 @@ class PointsController < ApplicationController
 		api_response(@points_json)
 	end
 
+	# Show data for a particular point whose id matches that given in
+	# the params
+	# Include Urls to mutate rank and delete record
 	def show
 		@point = Point.includes(:data).find(params[:id])
 		point_data = PointDatum.where(point_id: params[:id]).order('rank').map do |pd|
@@ -44,6 +52,8 @@ class PointsController < ApplicationController
 		api_response(items)
 	end
 
+	# Update a given point whose id matches that given
+	# in the params
 	def update
 		@point = Point.find(params[:id])
 		if @point.update_attributes(point_params)
@@ -53,12 +63,18 @@ class PointsController < ApplicationController
 		end
 	end
 
+	# Delete a given point whose id matches that
+	# given in the params
 	def destroy
 		TourPoint.where(point_id: params[:id]).destroy_all
 		Point.destroy(params[:id])
 		render json: ['Succesfully deleted point']
 	end
 
+	# Create a Point record, saving it to the database
+	# Ensure point has a cover photo file
+	# Analyse the file uploaded to ensure it's of the
+	# required format
 	def create
 		# Redirect back since no file provided
 		return redirect_to points_path if params[:file].nil?
@@ -81,6 +97,7 @@ class PointsController < ApplicationController
 
 		private
 
+	# Require a record of type Point and permit the given attributes
 	def point_params
 		params.require(:point).permit(:name, :description, :url)
 	end
