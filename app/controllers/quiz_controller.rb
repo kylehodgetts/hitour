54343f961f1feb418e2baaebfcd81765a2e8e42c
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-17 00:52:47 +0000

    Quiz docs

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,5 +1,11 @@
+# Version 1.0
+# Quiz Controller responsible for creating and manipulating
+# quiz records that will appear in one or more tours
 class QuizController < ApplicationController
   before_action :authenticate_user!
+
+  # Prepare all quiz records for response via the
+  # hiTour API
   def index
     @quiz = Quiz.includes(:questions).map do |quiz|
       quiz.as_json.merge(
@@ -11,6 +17,9 @@ class QuizController < ApplicationController
     api_response(@quiz)
   end
 
+  # Show a single Quiz whose id matches that
+  # given in the params
+  # Return the quiz with its collection of questions
   def show
     @quiz = Quiz.includes(:questions).find(params[:id])
     questions = quiz_questions
@@ -19,6 +28,9 @@ class QuizController < ApplicationController
     api_response(@quiz)
   end
 
+  # Create a Quiz record, saving it to the database
+  # Return a success message, if record is saved
+  # Otherwise, return an error message
   def create
     quiz = Quiz.new(quiz_params)
     quiz.save
@@ -31,6 +43,11 @@ class QuizController < ApplicationController
 	  render json: ['That name has already been used.']
   end
 
+  # Add the current quiz whose id matches that
+  # given in the quiz params to the tour given
+  # in the quiz params
+  # Return a Successful message if the association is made
+  # return an error message otherwise
   def add_tour_quiz
     tour = Tour.find(params[:tour][:id])
     quiz = Quiz.find(params[:quiz][:id])
@@ -42,6 +59,10 @@ class QuizController < ApplicationController
     end
   end
 
+  # Remove the Tour Quiz pair whose id matches that
+  # given in the params
+  # Return a Successful message if the association is destroyed
+  # return an error message otherwise
   def remove_tour_quiz
     tour_quiz = TourQuiz.find(params[:id]).delete
     if tour_quiz
@@ -51,6 +72,10 @@ class QuizController < ApplicationController
     end
   end
 
+  # Update a given quiz whose id matches that
+  # given in the params
+  # Return a success message, if record is updated
+  # Otherwise, return an error message
   def update
     quiz = Quiz.find(params[:id])
     if quiz.update_attributes(quiz_params)
@@ -60,6 +85,8 @@ class QuizController < ApplicationController
     end
   end
 
+  # Destroy the quiz whose id matches that
+  # given in the params
   def destroy
     Quiz.find(params[:id]).destroy
     render json: ['Deleted quiz']
@@ -67,6 +94,7 @@ class QuizController < ApplicationController
 
   private
 
+  # Require a record of type Quiz and permit the given attribute
   def quiz_params
     params.require(:quiz).permit(:name)
   end
