2e8060ac982bb51d0c21543ead43d525745fe87d
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-17 01:30:31 +0000

    Removed unused helper files

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -1,4 +1,11 @@
+# Version 1.0
+# Tour Controller Helper Methods
 module ToursHelper
+  # Remove all sessions where the current date
+  # is greater than the start date + the duration amount
+  # in days
+  # Essentially saying to remove all sessions
+  # past their expiry date
   def delete_expired_sessions
     TourSession.destroy_all(['start_date + duration < ?', Date.current])
   end
