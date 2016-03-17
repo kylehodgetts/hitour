df067572f73ecc9a94920ffbecc1bdb59e0ed653
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-03-17 01:03:05 +0000

    Session Controller tests

--- app/controllers/tour_sessions_controller.rb
+++ app/controllers/tour_sessions_controller.rb
@@ -1,6 +1,12 @@
+# Version 1.0
+# Tour Sessions Controller responsible for creating and maintaining
+# Tour Session relationships
 class TourSessionsController < ApplicationController
   before_action :authenticate_user!
 
+  # Save a Data Audience pair to the database
+  # Raise a RecordNotUnique exception if a relationship already exists
+  # between a given data and audience
   def create
     # Generate random passphrase if not given
     generate_passphrase
@@ -17,13 +23,10 @@ class TourSessionsController < ApplicationController
 		end
   end
 
-  def generate_passphrase
-    if params[:tour_session][:passphrase].nil?
-      random_word = RandomWord.nouns.next + SecureRandom.hex(3)
-      params[:tour_session][:passphrase] = random_word
-    end
-  end
-
+  # Update a given TourSession whose id matches that
+  # given in the params
+  # Return a success message, if record is updated
+  # Otherwise, return an error message
   def update
     tour_session = TourSession.find(params[:id])
     if tour_session.update_attributes(tour_session_params)
@@ -33,12 +36,17 @@ class TourSessionsController < ApplicationController
     end
   end
 
+  # Destroy a Tour Session pair whose relationship record
+  # matches the given id.
   def destroy
     tour_session = TourSession.find(params[:id])
     tour_session.destroy
     render json: ['Succesfully deleted tour session']
   end
 
+  # Send email to the given email in the params
+  # This will give the owner of the email address
+  # access to the CMS
   def send_email
     email = params[:email]
     tour_session = TourSession.find(params[:id])
@@ -47,6 +55,24 @@ class TourSessionsController < ApplicationController
     render json: ['Couldnt send email']
   end
 
+  private
+
+  # Require a record of type TourSession and permit the given attributes
+  def tour_session_params
+    params.require(:tour_session)
+          .permit(:tour_id, :name, :start_date, :duration, :passphrase)
+  end
+
+  # Generate a passphrase to allow mobile clients
+  # to access the associated tour
+  def generate_passphrase
+    if params[:tour_session][:passphrase].nil?
+      random_word = RandomWord.nouns.next + SecureRandom.hex(3)
+      params[:tour_session][:passphrase] = random_word
+    end
+  end
+
+  # Prepare invitation email to send to the given email
   def send_invitation_email(email, tour_session)
     @tour_session = tour_session
     email = SendGrid::Mail.new do |m|
@@ -56,12 +82,5 @@ class TourSessionsController < ApplicationController
      m.html = render_to_string(action: 'email_template', layout: false)
     end
     sendgrid.send(email)
-  end
-
-  private
-
-  def tour_session_params
-    params.require(:tour_session)
-          .permit(:tour_id, :name, :start_date, :duration, :passphrase)
   end
 end
